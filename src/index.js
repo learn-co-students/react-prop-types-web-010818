39872786b73738e899bf8c523b401eb0ae1a3419
Fei <fei.hafferkamp@gmail.com>
2018-03-11 14:58:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Order from './components/Order';
+
+const scoops = ['hazelnut', 'chocolate', 'peanut butter', 'pistachio']
+const orderInfo = {customerName: 'Fei', orderedAt: 1520794656}
+
+ReactDOM.render(
+  <Order
+    scoops={scoops}
+    orderInfo={orderInfo}
+  />,
+  document.getElementById('root')
+)
